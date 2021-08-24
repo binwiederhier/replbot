e5b0cdc6f2eb46a782ec31dc190d3c2071519a7b
Philipp Heckel <pheckel@datto.com>
2021-08-24 16:25:04 -0400

    Fix share client script to work on a mac

--- bot/share_server.sh
+++ bot/share_server.sh
@@ -6,7 +6,7 @@ case "$1" in
     echo "Hi there. Please run this command to create a local tmux session"
     echo "and connect it to REPLbot. Your tmux session will be shared here."
     echo
-    echo "bash -c "'"'"\$(ssh -p ${REPLBOT_SSH_PORT} ${REPLBOT_SESSION_ID}@${REPLBOT_SSH_HOST})"'"'
+    echo "bash -c "'"'"\$(ssh -T -p ${REPLBOT_SSH_PORT} ${REPLBOT_SESSION_ID}@${REPLBOT_SSH_HOST})"'"'
     echo
     echo "Waiting for client to connect ..."
     cleanup() {
